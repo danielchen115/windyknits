819d0797e7b6b9d25f2e24fee223da31a5193d70
Daniel Chen <daniel.chen115@gmail.com>
2026-05-19 11:48:21 -0400

    Initial Commit

---
+++ WindyKnits/WindyKnitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindyKnitsApp.swift
+//  WindyKnits
+//
+//  Created by Daniel Chen on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct WindyKnitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
